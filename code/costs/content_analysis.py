b2a01d2f34cb71908d0eb76a83f44a80fb21f626
Philippe Lammerts <philippelammerts@gmail.com>
2022-05-16 21:50:18 +0200

    Add filtering and comments

--- code/costs/content_analysis.py
+++ code/costs/content_analysis.py
@@ -1,7 +1,4 @@
 from sklearn.cluster import KMeans
-from sklearn.decomposition import TruncatedSVD
-from sklearn.preprocessing import Normalizer
-from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 import csv
 import preprocessor as p
@@ -10,44 +7,48 @@ import html
 # Implementation is based on:
 # https://scikit-learn.org/stable/auto_examples/text/plot_document_clustering.html#clustering
 
-K = 10
+# Number of clusters
+K = 8
+# The nu be
 N_COMPONENTS = 100
+# Filter on tweets that are hateful ('1') or not hateful ('0')
+HS = '0'
+# If HS is set to hateful, then we can filter on targeted to a specific individual ('1') or a generic group ('0')
+TR = '0'
+# If HS is set to hateful, then we can filter on aggressive tweets ('1') or non-aggressive tweets ('0')
+AG = '0'
+# Path of CSV file
 FILE_PATH = "F:\Thesis\data\SemEval\hateval2019_en_train.csv"
 
 with open(FILE_PATH, newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-filtered_data = list(filter(lambda x: x[2] == '0', data))
+# Filter out
+filtered_data = list(
+    filter(lambda x: x[2] == HS and x[3] == TR and x[4] == AG, data))
 print("size: ", len(filtered_data))
 
+# Remove html attributes and clean tweets by removing hashtags, mentions, and urls
 data = list(map(lambda x: p.clean(html.unescape(x[1])), filtered_data[1:]))
-print(data[:10])
-vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words="english")
 
+# Fit and transform the data using TF-IDF
+vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words="english")
 X = vectorizer.fit_transform(data)
 
-svd = TruncatedSVD(n_components=N_COMPONENTS)
-normalizer = Normalizer(copy=False)
-lsa = make_pipeline(svd, normalizer)
-
-X = lsa.fit_transform(X)
-
+# Apply k-means clustering
 km = KMeans(
     n_clusters=K,
     init="k-means++",
-    max_iter=100,
-    n_init=1,
 )
 pred = km.fit_predict(X)
 
+# Print top 10 terms per cluster
 print("Top terms per cluster:")
-original_space_centroids = svd.inverse_transform(km.cluster_centers_)
-order_centroids = original_space_centroids.argsort()[:, ::-1]
+ordered_centroids = km.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names_out()
-
 for i in range(K):
     print("Cluster %d:" % i, end="")
-    for ind in order_centroids[i, :10]:
+    for ind in ordered_centroids[i, :10]:
         print(" %s" % terms[ind], end="")
     print()
