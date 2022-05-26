96b83ce29698069647aa4cc9e5b91fb080174457
Philippe Lammerts <philippelammerts@gmail.com>
2022-05-26 12:47:39 +0200

    Add separate sem-eval notebooks for each configuration

--- code/costs/survey_data.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import random
-
-file = open("sem_eval_all.pkl", "rb")
-data = pickle.load(file)
-file.close()
-hate = list(filter(lambda d: d['label'] == 'hate', data))
-random_hate = random.choice(hate)
-print(random_hate)
