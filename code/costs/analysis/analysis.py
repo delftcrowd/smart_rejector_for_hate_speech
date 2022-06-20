6a502d0b9d5e15d4888a0b7f70795b30d9264f7a
Philippe Lammerts <philippelammerts@gmail.com>
2022-06-20 15:23:09 +0200

    Add me results to batch1 notebook

--- code/costs/analysis/analysis.py
+++ code/costs/analysis/analysis.py
@@ -142,7 +142,7 @@ class Analysis:
 
         for index, row in data.iterrows():
             mes = magnitude_estimates.iloc[[index]]
-            pivot = cls.pivot_value(row)
+            pivot = np.max(np.abs(mes.values))
             normalized_mes = mes.div(pivot)
             new_df = new_df.append(normalized_mes, ignore_index=True)
 
@@ -360,14 +360,8 @@ class Analysis:
         """
         NAME = "G20Q51"
         str_dis = row.filter(regex=fr"^{NAME}\[SQ001\]\.").values[0]
-        dis = row.filter(regex=fr"^{NAME}\[SQ002\]\.").values[0]
-        som_dis = row.filter(regex=fr"^{NAME}\[SQ003\]\.").values[0]
-        som_agr = row.filter(regex=fr"^{NAME}\[SQ005\]\.").values[0]
-        agr = row.filter(regex=fr"^{NAME}\[SQ006\]\.").values[0]
         str_agr = row.filter(regex=fr"^{NAME}\[SQ007\]\.").values[0]
-        calibration_vals = [str_dis, som_dis, dis, som_agr, agr, str_agr]
-        absolute_cal_vals = [abs(val) for val in calibration_vals]
-        return np.mean(absolute_cal_vals)
+        return np.max([str_dis, str_agr])
 
     @staticmethod
     def append_durations(data: pd.DataFrame) -> pd.DataFrame:
