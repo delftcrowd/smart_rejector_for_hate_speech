54ebd930dbd949aa80bca3b84e8f9896ebe336e8
Philippe Lammerts <philippelammerts@gmail.com>
2022-08-26 15:35:30 +0200

    Add general preprocessing functions with hashtags splitter

--- code/models/reader.py
+++ code/models/reader.py
@@ -2,16 +2,19 @@ from __future__ import annotations
 from sklearn.model_selection import train_test_split
 
 import pickle
-import numpy as np
+import preprocessor as p
+import html
+import regex as re
+from wordsegment import load, segment
+
+load()
 
 
 class Reader:
-    """Helper class for reading and splitting the text data."""
+    """Helper class for reading, preprocessing, and splitting the text data."""
 
-    def __init__(self, filename: str) -> None:
-        self.filename = filename
-
-    def split(self, X: list, y: list) -> tuple[list, list, list, list]:
+    @staticmethod
+    def split(X: list, y: list) -> tuple[list, list, list, list]:
         """Splits a list of data samples X and list of labels Y into a train and
         test dataset.
 
@@ -27,8 +30,17 @@ class Reader:
         )
         return X_train, X_test, y_train, y_test
 
+    @classmethod
+    def preprocess(cls, X: list) -> list:
+        p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.EMOJI, p.OPT.SMILEY)
+
+        return list(
+            map(lambda text: p.tokenize(html.unescape(cls.split_hashtags(text))), X)
+        )
+
+    @staticmethod
     def split_with_validation(
-        self, X: list, y: list
+        X: list, y: list
     ) -> tuple[list, list, list, list, list, list]:
         """Splits a list of data samples X and list of labels Y into a train,
         test, and validation dataset.
@@ -48,13 +60,14 @@ class Reader:
         )
         return X_train, X_val, X_test, y_train, y_val, y_test
 
-    def load(self) -> tuple[list, list]:
+    @staticmethod
+    def load(filename: str) -> tuple[list, list]:
         """Loads the data from an external file.
 
         Returns:
             tuple[list, list]: list of data samples and a list of labels.
         """
-        data = pickle.load(open(self.filename, "rb"))
+        data = pickle.load(open(filename, "rb"))
         X = []
         y = []
         for i in range(len(data)):
@@ -62,3 +75,11 @@ class Reader:
             y.append(data[i]["label"])
 
         return X, y
+
+    @staticmethod
+    def split_hashtags(text: str) -> str:
+        hashtags = re.findall(r"(#\w+)", text)
+        for hashtag in hashtags:
+            hashtag_words = " ".join(segment(hashtag.lstrip("#")))
+            text = text.replace(hashtag, hashtag_words)
+        return text
